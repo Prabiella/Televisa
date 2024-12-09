a8da50c40ab5256c13d5e82062bd71ba082d7942
Prabiella <60824115+Prabiella@users.noreply.github.com>
2024-12-09 14:40:19 -0600

    ajustes

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,16 @@
-//Menu hamburguesa
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-const container = document.querySelector('.menu-container');
+document.addEventListener('DOMContentLoaded', function () {
+  // Elementos
+  const hamburger = document.getElementById('hamburger');
+  const menu = document.getElementById('menu');
+  const container = document.querySelector('.menu-container');
+  const body = document.querySelector('body');
 
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  container.classList.toggle('active');
-  hamburger.classList.toggle('active');
+  // Evento de click en el hamburguesa
+  hamburger.addEventListener('click', () => {
+    // Togglear clases en el menú, contenedor y botón hamburguesa
+    menu.classList.toggle('active');
+    container.classList.toggle('active');
+    body.classList.toggle('menu-open');
+    hamburger.classList.toggle('active');
+  });
 });
